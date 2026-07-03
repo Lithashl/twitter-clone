e5c2564bac51da92a3dc3beafada899cc65d6ac3
Lithashl <lithashl04@gmail.com>
2026-07-03 19:42:02 +0700

    1- auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -9,7 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse req.body for form submissions
+
+app.use(cookieParser()); // to parse cookies
 
 app.use("/api/auth", authRoutes);
 
