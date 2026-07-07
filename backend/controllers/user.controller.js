bb77fa0df94ff3ebc9d15022ba0cb8510dacce11
Lithashl <lithashl04@gmail.com>
2026-07-07 07:08:37 +0700

    1- users routes and controllers added

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,153 @@
+import bcrypt from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary";
+
+// models
+import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
+
+
+export const getUserProfile = async (req, res, next) => {
+    const {username} = req.params;
+
+    try {
+        const user = await User.findOne({username}).select("-password");
+        if(!user) {
+            return res.status(404).json({error: "User not found"});
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.error("Error in getUserProfile:", error.message);
+        res.status(500).json({error: "Server error"});
+    }
+}
+
+export const followUnfollowUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userToModify = await User.findById(id);   
+        const currentUser = await User.findById(req.user._id);
+
+        if (id === req.user._id.toString()) {
+            return res.status(400).json({ error: "You cannot follow/unfollow yourself" });
+        }
+
+        if (!userToModify || !currentUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const isFollowing = currentUser.following.includes(id);
+
+        if (isFollowing) {
+            // Unfollow user
+            await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+            await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+
+            res.status(200).json({ message: "Successfully unfollowed user" });
+        } else {
+            // Follow user
+            await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+            await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+
+            // send notification to the user
+            const newNotification = new Notification({
+                type: "follow",
+                from: req.user._id,
+                to: userToModify._id,   
+            });
+            await newNotification.save();   
+
+            res.status(200).json({ message: "Successfully followed user" });
+        }
+
+    } catch (error) {
+        console.error("Error in followUnfollowUser:", error.message);
+        res.status(500).json({ error: "Server error" });
+    }
+}
+
+export const getSuggestedUsers = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const usersFollowedByMe = await User.findById(userId).select("following");
+
+        const users = await User.aggregate([
+            {
+                $match: {
+                    _id: { $ne: userId }, // Exclude the current user
+                }
+            },
+            {$sample: {size: 10}}
+        ])
+
+        // 1, 2, 3, 4, 5, 6,
+        const filteredUsers = users. filter(user => !usersFollowedByMe.following.includes(user._id));
+        const suggestedUsers = filteredUsers.slice(0.4)
+
+        suggestedUsers.forEach(user=>user.password = null)
+        res.status(200).json(suggestedUsers);
+    } catch (error) {
+        console.log("Error in getSuggestedUsers:", error.message);
+        res.status(500).json({ error: "Server error" });
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const {fullName, email, username, currentPassword, newPassword, bio, link} = req.body;
+    let {profileImg, coverImg} = req.body;
+
+    const userId = req.user._id;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ error: "User not found" });
+
+        if((!newPassword && currentPassword) || (!currentPassword && newPassword)) {
+            return res.status(400).json({ error: "Both current and new passwords are required" });
+        }
+
+        if(currentPassword && newPassword) {
+            const isMatch = await bcrypt.compare(currentPassword, user.password);
+            if(!isMatch) return res.status(400).json({ error: "Current password is incorrect" });
+            if(newPassword.length < 6) {
+                return res.status(400).json({ error: "Password must be at least 6 characters long" });
+            }
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(newPassword, salt);
+        }
+
+        if(profileImg) {
+            if (user.profileImg){
+                await cloudinary.uploader.destroy(user.profileImg.split("/").pop().split(".")[0]); // delete the old image from Cloudinary
+            }
+            const uploadResponse = await cloudinary.uploader.upload(profileImg);
+            profileImg = uploadResponse.secure_url;
+        }
+
+        if(coverImg) {
+            if (user.coverImg){
+                await cloudinary.uploader.destroy(user.coverImg.split("/").pop().split(".")[0]); // delete the old image from Cloudinary
+            }
+            const uploadResponse = await cloudinary.uploader.upload(coverImg);
+            coverImg = uploadResponse.secure_url;
+        }
+
+        user.fullName = fullName || user.fullName;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.bio = bio || user.bio;
+        user.link = link || user.link;
+        user.profileImg = profileImg || user.profileImg;
+        user.coverImg = coverImg || user.coverImg;
+
+        await user.save();
+
+        user.password = null;
+
+        return res.status(200).json(user);
+    } catch (error) {
+        console.log("Error in updateUser:", error.message);
+        return res.status(500).json({ error: "Server error" });
+
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
+
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
+
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +30,8 @@ app.use(express.urlencoded({ extended: true })); // to parse req.body for form s
 app.use(cookieParser()); // to parse cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 
 
 
