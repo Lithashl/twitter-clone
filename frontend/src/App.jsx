74d881c7e638690465a727c43193ca86842bc9f4
Lithashl <lithashl04@gmail.com>
2026-07-23 20:29:15 +0700

    2-UI Design Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/auth/login/LoginPage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
+import ProfilePage from './pages/profile/ProfilePage';
+import NotificationPage from './pages/notification/NotificationPage';
+
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel'; 
+
+
 
 function App() {
   return (
@@ -15,6 +20,8 @@ function App() {
         <Route path='/' element={<HomePage/>} />
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/signup' element={<SignUpPage/>} />
+        <Route path='/notifications' element={<NotificationPage/>}/>
+        <Route path='/profile/:username' element={<ProfilePage/>}/>
       </Routes>
       <RightPanel></RightPanel>
     </div>
